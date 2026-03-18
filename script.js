3fff4e1f3690e6a638c26d38e0d9481570cc7e9f
Nolanxin <3191293707@qq.com>
2026-03-18 23:56:56 +0800

    更新research，以及一大堆...

--- script.js
+++ script.js
@@ -122,3 +122,45 @@ function initPageSpecificFeatures() {
 document.addEventListener('DOMContentLoaded', function() {
     initPageSpecificFeatures();
 });
+
+/**
+ * 轮播图功能
+ */
+let slideIndex = 1;
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("slideshow-slide");
+    const dots = document.getElementsByClassName("slideshow-dot");
+    
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+// 自动轮播
+setInterval(function() {
+    plusSlides(1);
+}, 5000);
